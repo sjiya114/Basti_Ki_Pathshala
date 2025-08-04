d3dbf564d910749e7a16335ae2cd64200003ae72
sjiya114 <sjiya04072004@gmail.com>
2025-08-04 22:49:38 +0530

    animating

--- frontend/src/components/Join.jsx
+++ frontend/src/components/Join.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import './Join.css';
 
 function Join() {
-    const navigate=useNavigate();
+    const navigate = useNavigate();
+    const paragraph = "At Basti Ki Pathshala Foundation,we are not  just building a team; we’re assembling  a force for change. As we continue our  mission to break barriers and empower communities through  education, we invite passionate individuals to join us in  making a tangible difference in the lives of underserved children. Whether you’re a seasoned educator, a community organizer, or simply someone with a heart for social impact, there’s a place for you in our team. Together, we’ll collaborate, innovate, and inspire, working towards a future where every child has the opportunity to thrive.Join us in this rewarding journey of transformation. Together,let’s pave the way for brighter tomorrows and empower futures, one child at a time."
+    const parag=paragraph.split(" ");
     return (
         <div id='join' className='flex flex-row  max-md:flex-col mt-30 hover:scale-110 max-w-screen-xl mx-auto'>
             <div className='w-1/2 max-md:w-full'>
@@ -11,7 +14,7 @@ function Join() {
             </div>
             <div className='w-1/2 max-md:w-full text-white px-6 py-4'>
                 <h1 className='font-bold text-3xl'>Join Our Team</h1>
-                <p className='mt-6 tracking-wide'>At Basti Ki Pathshala Foundation, we’re not
+                {/* <p id='para' className='mt-6 tracking-wide '>At Basti Ki Pathshala Foundation, we’re not
                     just building a team; we’re assembling
                     a force for change. As we continue our
                     mission to break barriers and empower communities through
@@ -22,10 +25,17 @@ function Join() {
                     for you in our team. Together, we’ll collaborate, innovate,
                     and inspire, working towards a future where every child has the opportunity to thrive.
                     Join us in this rewarding journey of transformation. Together,
-                    let’s pave the way for brighter tomorrows and empower futures, one child at a time.</p>
-                      <button onClick={()=>{navigate("/register")}}  className='border-2 border-yellow-400 font-bold hover:bg-yellow-400 mt-6 px-2 py-2 rounded-lg bg-white text-black'>Join Now</button>
+                    let’s pave the way for brighter tomorrows and empower futures, one child at a time.</p> */}
+                  <div className='tracking-wider'>
+                     {parag.map((data,index)=>(
+                    <p  style={{animationDelay:`${index*0.05}s`}} className='para inline opacity-0'>{data+" "}</p>
+                 ))
+                 }
+                  </div>
+                
+                <button onClick={() => { navigate("/register") }} className='border-2 border-yellow-400 font-bold hover:bg-yellow-400 mt-6 px-2 py-2 rounded-lg bg-white text-black'>Join Now</button>
             </div>
-          
+
         </div>
     )
 }
--- frontend/src/components/ReviewCard.jsx
+++ frontend/src/components/ReviewCard.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function ReviewCard({description,profile}) {
   return (
-      <div className='mt-20 mb-10 max-w-screen-xl mx-auto flex flex-wrap items-center justify-center' >
+      <div className='mt-20 mb-10   hover:scale-105 max-w-screen-xl mx-auto flex flex-wrap items-center justify-center' >
       <div className='flex flex-row  max-md:flex-col max-md:w-[60%]   shadow-2xl shadow-white w-[640px] h-[300px] bg-black space-x-4 border-1  max-md:h-fit border-white rounded-lg'>
-        <img src={profile} className='w-fit   max-md:w-full h-fit rounded-lg' alt="review" />
+        <img src={profile} className='w-fit   max-md:w-full h-full rounded-lg' alt="review" />
         <div className='text-white max-md:py-6 px-4 flex flex-col space-y-10 items-center justify-center'>
           <p>⭐⭐⭐⭐⭐</p>
           <p className='text-sm font-semibold'>{description}</p>
